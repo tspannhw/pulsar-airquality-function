64dfa0b8c9bd06c7789c9304352418bd8c3fa40f
tspannhw <tim.spann@streamnative.io>
2022-05-19 16:07:25 +0200

    Update

---
+++ copy.sh
@@ -0,0 +1,2 @@
+cp target/airquality-1.0.jar /Users/tspann/Documents/code/apache-pulsar-2.10.0/
+
--- src/test/java/dev/pulsarfunction/airquality/AirQualityFunctionTest.java
+++ src/test/java/dev/pulsarfunction/airquality/AirQualityFunctionTest.java
@@ -96,8 +96,15 @@ public class AirQualityFunctionTest {
                 .autoAck(true)
                 .build();
 
+        /**
+         *
+         *
+         *  .clientAuthParams("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5rRXdSVVU1TUVOQlJrWTJNalEzTVRZek9FVkZRVVUyT0RNME5qUkRRVEU1T1VNMU16STVPUSJ9.eyJodHRwczovL3N0cmVhbW5hdGl2ZS5pby91c2VybmFtZSI6InRzcGFubkBzbmRlbW8uYXV0aC5zdHJlYW1uYXRpdmUuY2xvdWQiLCJpc3MiOiJodHRwczovL2F1dGguc3RyZWFtbmF0aXZlLmNsb3VkLyIsInN1YiI6IkNJRzFGTlNmRnNiOXMyc2lSRHNoY0xCZE1zNWtEMUxzQGNsaWVudHMiLCJhdWQiOiJ1cm46c246cHVsc2FyOnNuZGVtbzpkZW1vLWNsdXN0ZXIiLCJpYXQiOjE2NDk5MzQ4MjksImV4cCI6MTY1MDUzOTYyOSwiYXpwIjoiQ0lHMUZOU2ZGc2I5czJzaVJEc2hjTEJkTXM1a0QxTHMiLCJzY29wZSI6ImFkbWluIGFjY2VzcyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsInBlcm1pc3Npb25zIjpbImFkbWluIiwiYWNjZXNzIl19.hXmqplmhxVD-hFrtEJSJSOgNDq0dwxFntfz4PXNsKrDBQZe2gxPfztR13cdpeDJzlza_rCOLfFCqCqzcnV1DJFnoSpsVNjygBrHkiTXKxufDuyvvReLYDujtMuDUl5mr-y5IlKpeiUEIbk3RaW_AVVP6XCzxxqlXNQBq8OLJ3IpYFowM1TRjgEIvHw-K9f-j_BxNDaFTzi1VE40_zUZ1mDA2u9QFre-zhJ84j_Wjj-qdXWygd4BI2lj4MEtK6FODVQVab_cStHYNtx7mA7wk2KsPkXoJ1qWLrL8NjlS7YjLd69vJQTyAkGqzTgr3K5R3TIOinA6KG6ZzJqQmdZhBKQ")
+         */
         LocalRunner localRunner = LocalRunner.builder()
-                .brokerServiceUrl("pulsar://pulsar1:6650")
+                .brokerServiceUrl("pulsar+ssl://demo.sndemo.snio.cloud:6651")
+                .clientAuthPlugin("org.apache.pulsar.client.impl.auth.AuthenticationToken")
+                .clientAuthParams("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5rRXdSVVU1TUVOQlJrWTJNalEzTVRZek9FVkZRVVUyT0RNME5qUkRRVEU1T1VNMU16STVPUSJ9.eyJodHRwczovL3N0cmVhbW5hdGl2ZS5pby91c2VybmFtZSI6InRzcGFubkBzbmRlbW8uYXV0aC5zdHJlYW1uYXRpdmUuY2xvdWQiLCJpc3MiOiJodHRwczovL2F1dGguc3RyZWFtbmF0aXZlLmNsb3VkLyIsInN1YiI6IkNJRzFGTlNmRnNiOXMyc2lSRHNoY0xCZE1zNWtEMUxzQGNsaWVudHMiLCJhdWQiOiJ1cm46c246cHVsc2FyOnNuZGVtbzpkZW1vLWNsdXN0ZXIiLCJpYXQiOjE2NDk5MzQ4MjksImV4cCI6MTY1MDUzOTYyOSwiYXpwIjoiQ0lHMUZOU2ZGc2I5czJzaVJEc2hjTEJkTXM1a0QxTHMiLCJzY29wZSI6ImFkbWluIGFjY2VzcyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsInBlcm1pc3Npb25zIjpbImFkbWluIiwiYWNjZXNzIl19.hXmqplmhxVD-hFrtEJSJSOgNDq0dwxFntfz4PXNsKrDBQZe2gxPfztR13cdpeDJzlza_rCOLfFCqCqzcnV1DJFnoSpsVNjygBrHkiTXKxufDuyvvReLYDujtMuDUl5mr-y5IlKpeiUEIbk3RaW_AVVP6XCzxxqlXNQBq8OLJ3IpYFowM1TRjgEIvHw-K9f-j_BxNDaFTzi1VE40_zUZ1mDA2u9QFre-zhJ84j_Wjj-qdXWygd4BI2lj4MEtK6FODVQVab_cStHYNtx7mA7wk2KsPkXoJ1qWLrL8NjlS7YjLd69vJQTyAkGqzTgr3K5R3TIOinA6KG6ZzJqQmdZhBKQ")
                 .functionConfig(functionConfig)
                 .build();
 
